c661df5ff475ae522edab1c7f9b8c7c7c830c107
Mobin Chowdhury <47663360+MobinX@users.noreply.github.com>
2024-05-05 16:41:39 +0000

    try

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { getURL } from "@/services/getUrl";
+export const runtime = 'edge';
 export default async function Home() {
   return (
     <main>{(await getURL()).href}</main>
